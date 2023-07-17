c84aa402d8bfe7be834e93855df9afff72bba816
jmbhcabral <larbak.cstrike@gmail.com>
2023-07-17 22:32:02 +0100

    Settings and configs for django

---
+++ scripts/makemigrations.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo 'Executando Makemigrations.sh'
+python manage.py makemigrations --noinput
---
+++ scripts/migrate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo 'Executando Migrate.sh'
+python manage.py migrate --noinput
