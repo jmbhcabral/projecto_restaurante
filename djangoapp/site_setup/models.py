b1ae90fc33c1834aa61d88eb3c394ce0a231e225
jmbhcabral <larbak.cstrike@gmail.com>
2023-07-19 20:23:08 +0100

    Favicon, imge resizing

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.model_validators import validate_png
+from utils.images import resize_image
 
 
 class MenuLink(models.Model):
@@ -35,3 +37,23 @@ class SiteSetup(models.Model):
     show_reservations = models.BooleanField(default=True)
     show_takeaway = models.BooleanField(default=True)
     show_delivery = models.BooleanField(default=True)
+
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/',
+        blank=True, default='',
+        validators=[validate_png],
+    )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
+
+    def __str__(self):
+        return self.title
