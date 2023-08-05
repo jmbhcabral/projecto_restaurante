cb3c04d8d1c378906151b4cd02164273d1bbd019
jmbhcabral <larbak.cstrike@gmail.com>
2023-08-05 22:54:41 +0100

    Frontend development

--- djangoapp/utils/images.py
+++ djangoapp/utils/images.py
@@ -1,16 +1,43 @@
 from pathlib import Path
 
 from django.conf import settings
-from PIL import Image
+from PIL import Image, ExifTags
+
+
+def get_exif_orientation(image_pillow):
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+        exif = image_pillow._getexif()
+        if exif is not None:
+            return exif[orientation]
+    except (AttributeError, KeyError, IndexError):
+        pass
+    return None
+
+
+def fix_orientation(image_pillow):
+    orientation = get_exif_orientation(image_pillow)
+    if orientation == 3:
+        return image_pillow.rotate(180, expand=True)
+    elif orientation == 6:
+        return image_pillow.rotate(270, expand=True)
+    elif orientation == 8:
+        return image_pillow.rotate(90, expand=True)
+    return image_pillow
 
 
 def resize_image(image_django, new_width=800, optimize=True, quality=60):
     image_path = Path(settings.MEDIA_ROOT / image_django.name).resolve()
     image_pillow = Image.open(image_path)
+
+    # Corrigir a orientação da imagem (se necessário)
+    image_pillow = fix_orientation(image_pillow)
+
     original_width, original_height = image_pillow.size
 
     if original_width <= new_width:
-        image_pillow.close()
         return image_pillow
 
     new_height = round(new_width * original_height / original_width)
