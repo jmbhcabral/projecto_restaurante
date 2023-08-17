3141dc0cf765eb3dc75bfd596751b3da6f2e4d31
jmbhcabral <larbak.cstrike@gmail.com>
2023-08-17 22:44:23 +0100

    Banckend development - Category and SunCategory forms

--- djangoapp/restau/forms.py
+++ djangoapp/restau/forms.py
@@ -1,6 +1,106 @@
 from django.core.exceptions import ValidationError
 from django import forms
-from restau.models import Products
+from restau.models import Products, Category, SubCategory
+
+
+class SubcategoryForm(forms.ModelForm):
+    nome = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'digite aqui',
+            }
+        ),
+        label='Nome',
+        help_text='Nome do artigo.'
+    )
+
+    ordem = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={
+                'placeholder': 'digite aqui',
+            }
+        ),
+        label='Ordem',
+        help_text='Ordem do artigo.'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = SubCategory
+        fields = (
+            'nome', 'ordem',
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome')
+        instance = self.instance
+
+        if instance and instance.nome == nome:
+            return cleaned_data
+        if Products.objects.filter(nome=nome).exists():
+
+            self.add_error(
+                'nome',
+                ValidationError(
+                    'O produto já existe.',
+                    code='invalid'
+                )
+            )
+
+        return super().clean()
+
+
+class CategoryForm(forms.ModelForm):
+    nome = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'digite aqui',
+            }
+        ),
+        label='Nome',
+        help_text='Nome do artigo.'
+    )
+
+    ordem = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={
+                'placeholder': 'digite aqui',
+            }
+        ),
+        label='Ordem',
+        help_text='Ordem do artigo.'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = Category
+        fields = (
+            'nome', 'ordem',
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome')
+        instance = self.instance
+
+        if instance and instance.nome == nome:
+            return cleaned_data
+        if Products.objects.filter(nome=nome).exists():
+
+            self.add_error(
+                'nome',
+                ValidationError(
+                    'O produto já existe.',
+                    code='invalid'
+                )
+            )
+
+        return super().clean()
 
 
 class ProductForm(forms.ModelForm):
