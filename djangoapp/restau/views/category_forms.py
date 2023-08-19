6d08d17bd0c3befbb7d1514b849a39ea25cddec6
Seu nome <seu_email@gmail.com>
2023-08-19 11:18:22 +0100

    Backend development - category order

--- djangoapp/restau/views/category_forms.py
+++ djangoapp/restau/views/category_forms.py
@@ -2,25 +2,36 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from restau.forms import CategoryForm
 from restau.models import Category
+from django.forms import modelformset_factory
 
 
 def create_category(request):
+    CategoriaFormSet = modelformset_factory(
+        Category, fields=('ordem',), extra=0)
     categorias = Category.objects \
         .order_by('id')
 
+    formset = CategoriaFormSet(queryset=Category.objects.all())
+    print(CategoriaFormSet)
+
     form_action = reverse('restau:create_category')
-    print("Form Action:", form_action)
     if request.method == 'POST':
+        formset = CategoriaFormSet(request.POST)
         form = CategoryForm(request.POST, request.FILES)
         context = {
+            'formset': formset,
             'form': form,
             'form_action': form_action,
         }
 
         if form.is_valid():
             categoria = form.save()
-            return redirect('restau:create_category',
+            return redirect('restau:category',
                             category_id=categoria.id)
+
+        if formset.is_valid():
+            formset.save()
+            return redirect('restau:create_category',)
 
         return render(
             request,
@@ -30,6 +41,7 @@ def create_category(request):
 
     context = {
         'categorias': categorias,
+        'formset': formset,
         'form': CategoryForm(),
         'form_action': form_action,
     }
