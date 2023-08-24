ffa5f05ec310d00386484c3ea1fcb29fa6867d2d
Seu nome <seu_email@gmail.com>
2023-08-24 10:46:44 +0100

    Frontend development - Product listing with category and subcategory filters

--- djangoapp/restau/views/product_views.py
+++ djangoapp/restau/views/product_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from restau.models import Products, FrontendSetup
+from restau.models import Products, FrontendSetup, Category, SubCategory
 
 
 PER_PAGE = 9
@@ -40,8 +40,28 @@ def encomendas(request):
         .first()
 
     produtos = Products.objects \
-        .filter('categoria') \
+        .all() \
+        .order_by('id')
+
+    categorias = Category.objects \
+        .all() \
         .order_by('ordem')
+
+    subcategorias = SubCategory.objects \
+        .all() \
+        .order_by('ordem')
+
+    # # Obtém o ID da categoria da URL
+    # categoria_id = request.GET.get('categoria_id')
+    # # Obtém o ID da subcategoria da URL
+    # subcategoria_id = request.GET.get('subcategoria_id')
+
+    # if categoria_id and subcategoria_id:
+    #     produtos = Products.objects \
+    #         .filter(
+    #             categoria_id=categoria_id,
+    #             subcategoria_id=subcategoria_id) \
+    #         .order_by('id')
 
     return render(
         request,
@@ -50,7 +70,11 @@ def encomendas(request):
             'main_logo': main_logo,
             'main_image': main_image,
             'default_image': default_image,
-            'produtos': produtos
+            'produtos': produtos,
+            'categorias': categorias,
+            'subcategorias': subcategorias,
+            'categoria_id': int(request.GET.get('categoria_id', 0)),
+            'subcategoria_id': int(request.GET.get('subcategoria_id', 0)),
         },
     )
 
