1217a2c064d002bb47b76e2055a1faa1d1744811
Seu nome <seu_email@gmail.com>
2023-09-13 11:12:55 +0100

    Backend development - Ementa errors

--- djangoapp/restau/views/ementa_forms.py
+++ djangoapp/restau/views/ementa_forms.py
@@ -100,37 +100,62 @@ def ementas_delete(request, ementa_id):
 
 
 def povoar_ementa(request, ementa_id):
+    print('REQUEST: ', request.POST)
+    print("Estou aqui!")  # Isso deve ser impresso sempre que a view for chamada
+    # Isso vai imprimir o método HTTP da requisição
+    print("Método HTTP:", request.method)
+
     ementa = get_object_or_404(Ementa, pk=ementa_id)
 
     categorias = Category.objects.all().order_by('ordem')
     subcategorias = SubCategory.objects.all().order_by('ordem')
-    artigo = Products.objects.all().order_by('ordem')
+    produtos = Products.objects.all().order_by('ordem')
 
     form_action = reverse('restau:povoar_ementa', args=[ementa_id])
-
     if request.method == 'POST':
-        form = ProdutosEmentaForm(request.POST, ementa_id=ementa_id)
+        form = ProdutosEmentaForm(request.POST or None, ementa_id=ementa_id)
+        print("Formulário POST recebido")
+        print(request.POST)
+        try:
+            valid = form.is_valid()
+            print(valid)
+        except Exception as e:
+            print("Erro durante a validação: ", e)
+
         if form.is_valid():
-            form.save()
-            # Or wherever you'd like to redirect
+            print('Formulário é válido.')
+
+            ementa = form.cleaned_data['ementa']
+            print("Ementa: ", ementa)
+
+            selected_products = form.cleaned_data['produto']
+            print("Selected products: ", selected_products)
+
+            for product in form.cleaned_data['produto']:
+                ProdutosEmenta.objects.create(ementa=ementa, produto=product)
+
             return redirect('restau:povoar_ementa', args=[ementa_id])
+        else:
+            print("O formulário não é válido.")
+            print(form.errors)
 
         context = {
             'form': form,
             'form_action': form_action,
             'ementa': ementa,
-            'artigo': artigo,
+            'produtos': produtos,
             'categorias': categorias,
             'subcategorias': subcategorias,
         }
         return render(request, 'restau/pages/povoar_ementa.html', context)
 
     else:
+        print("Não foi uma solicitação POST.")
         form = ProdutosEmentaForm(ementa_id=ementa_id)
 
     context = {
         'ementa': ementa,
-        'artigo': artigo,
+        'produtos': produtos,
         'categorias': categorias,
         'subcategorias': subcategorias,
         'form': form,
