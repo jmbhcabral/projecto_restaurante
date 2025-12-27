1835a4796464f5aa8ccb29df52f850fa8fd0b75d
Jorge Cabral <larbak.cstrike@gmail.com>
2025-12-27 05:37:47 +0000

    refactor(settings): update context processor import path to include 'djangoapp' namespace for consistency

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -91,7 +91,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'site_setup.context_processors.site_setup'
+                'djangoapp.site_setup.context_processors.site_setup'
             ],
         },
     },
